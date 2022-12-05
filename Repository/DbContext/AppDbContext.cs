c1feb892f21bc495d6c8fe33f58bc27f09994849
salli <salli@MSI>
2022-12-05 16:58:00 +0300

    updated

--- Repository/DbContext/AppDbContext.cs
+++ Repository/DbContext/AppDbContext.cs
@@ -12,11 +12,16 @@ namespace Repository.DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
-
+            ///
         }
         public DbSet<Category> Categories  { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures  { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
 
     }
 }
