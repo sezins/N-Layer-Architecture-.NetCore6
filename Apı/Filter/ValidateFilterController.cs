beb64d021a2811061e6e8d80f81c29cbba6c7cce
salli <salli@MSI>
2022-12-10 17:22:48 +0300

    UPDATED

---
+++ Apı/Filter/ValidateFilterController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Filter
+{
+    public class ValidateFilterController : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+    }
+}
